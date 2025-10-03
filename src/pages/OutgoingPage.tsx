ac1f5ee48e7638145a9487f51d7805a024ba4dbc
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-10-02 20:26:07 -0600

    Add logic to see outgoings

--- src/pages/OutgoingPage.tsx
+++ src/pages/OutgoingPage.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import type { IShipmentModel } from "../models/shipment";
+import { ColumnsShipment } from "../components/ColumnsShipment";
+import { DataTable } from "../components/DataTable";
+import { getShipments } from "../services/ShipmentService";
+
 export default function OutgoingPage() {
+  const [shipments, setShipments] = useState<IShipmentModel[]>([]);
+  const [loadingShipments, setLoadingShipments] = useState(true);
+
+  useEffect(() => {
+    const fetchShipments = async () => {
+      try {
+        const data = await getShipments("origin");
+        setShipments(data);
+      } catch (error) {
+        console.error("Error al cargar los envios:", error);
+      } finally {
+        setLoadingShipments(false);
+      }
+    };
+
+    fetchShipments();
+  }, []);
+
   return(
-    <p>Outgoing page</p>
+    <>
+      <h1>Salientes</h1>
+      {loadingShipments ? 
+        <p>Cargando datos...</p> :
+        <div className="container py-10">
+          <DataTable columns={ColumnsShipment} data={shipments} />
+        </div>
+      }
+    </>
   )
 }
