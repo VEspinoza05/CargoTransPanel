9c217aa1d6a30f94b40cfe53ade9e52f9b130f75
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-10-02 20:23:49 -0600

    Add logic to see incoming

--- src/pages/IncomingPage.tsx
+++ src/pages/IncomingPage.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import type { IShipmentModel } from "../models/shipment";
+import { ColumnsShipment } from "../components/ColumnsShipment";
+import { DataTable } from "../components/DataTable";
+import { getShipments } from "../services/ShipmentService";
+
 export default function IncomingPage() {
+  const [shipments, setShipments] = useState<IShipmentModel[]>([]);
+  const [loadingShipments, setLoadingShipments] = useState(true);
+
+  useEffect(() => {
+    const fetchShipments = async () => {
+      try {
+        const data = await getShipments("destination");
+        //console.log(JSON.stringify(data));
+        setShipments(data);
+      } catch (error) {
+        console.error("Error al cargar los envios:", error);
+      } finally {
+        setLoadingShipments(false);
+      }
+    };
+
+    fetchShipments();
+  }, []);
+
   return(
-    <p>Incoming page</p>
+    <>
+      <h1>Entrantes</h1>
+      {loadingShipments ? 
+        <p>Cargando datos...</p> :
+        <div className="container py-10">
+          <DataTable columns={ColumnsShipment} data={shipments} />
+        </div>
+      }
+    </>
   )
 }
--- src/services/ShipmentService.ts
+++ src/services/ShipmentService.ts
@@ -1,7 +1,11 @@
 import axios from "../api/axios";
 import type { IShipmentModel } from "../models/shipment";
 
-export const getShipments = async (): Promise<IShipmentModel[]> => {
-  const response = await axios.get<IShipmentModel[]>("/Shipment")
+export const getShipments = async (isOriginOrDestination = ""): Promise<IShipmentModel[]> => {
+  const response = await axios.get<IShipmentModel[]>("/Shipment", {
+    params: {
+      isOriginOrDestination: isOriginOrDestination
+    }
+  })
   return response.data;
 };
