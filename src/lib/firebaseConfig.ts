26fbd1a99fbc1dd02c761dfa8059deb980be9074
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-10-03 08:24:06 -0600

    Hide Firebase api key in environment variable
    This has been done for security reasons.

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCMs22Pfa5ZvoWDBJbgUfCWtj5jVhmAsk",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "cargotrans-473716.firebaseapp.com",
   projectId: "cargotrans-473716",
   storageBucket: "cargotrans-473716.firebasestorage.app",
   messagingSenderId: "764923891343",
-  appId: "1:764923891343:web:af5bad0283212315baeafa",
-  measurementId: "G-BQ295PF93X"
+  appId: "1:764923891343:web:4696700970ac7dc8baeafa",
+  measurementId: "G-RQEGGH5PEX"
 };
 
 const app = initializeApp(firebaseConfig);
